6c5aacda1fb3ae93e8aa691b2c453464119b0014
Vincenzo Marcella <marcella.vincenzo2@gmail.com>
2021-11-03 16:56:12 +0100

    Also implemented anti aliasing

--- graphics.h
+++ graphics.h
@@ -26,6 +26,7 @@ void processInput(GLFWwindow* window) {
 
 GLFWwindow* createWindow(int width, int height) {
     glfwInit();
+    glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
