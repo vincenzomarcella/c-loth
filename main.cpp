a78b7656aa8c097c64a001fe4954317601e1d1b0
Vincenzo Marcella <marcella.vincenzo2@gmail.com>
2021-11-23 10:09:10 +0100

    Added unpin all function

--- main.cpp
+++ main.cpp
@@ -32,6 +32,23 @@ void processInput(GLFWwindow* window) {
         glfwSetWindowShouldClose(window, true);
 }
 
+
+// Unpin all points except corners
+void unpinAll(PointMass* points[]) {
+    // Unfixing all points
+    for (int i = 0; i < ROWS; i++)
+        for (int j = 0; j < COLS; j++) {
+            PointMass* point = points[i * COLS + j];
+            point->unfix_position();
+        }   
+
+    // Refixing corners
+    points[0]->fix_position();
+    points[COLS - 1]->fix_position();
+    points[COLS * (ROWS - 1)]->fix_position();
+    points[COLS * ROWS - 1]->fix_position();
+}
+
 int main() {
     PointMass* points[COLS * ROWS]{};
 
@@ -249,9 +266,9 @@ int main() {
             //ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
             //ImGui::ColorEdit3("clear color", (float*)&GUIState->clear_color); // Edit 3 floats representing a color
 
-            /*if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
+            if (ImGui::Button("Unpin all"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                unpinAll(points);
+            /*ImGui::SameLine();
             ImGui::Text("counter = %d", counter);*/
 
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
